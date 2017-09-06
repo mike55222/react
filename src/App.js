df7fa6b02ec2159098066a76f9a5761335acdacc
MDmike <mike55222@gmail.com>
2017-09-06 15:44:55 +0700

    class32

--- src/App.js
+++ src/App.js
@@ -1,59 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header.components';
-import Footer from './components/Footer.components';
-import Cal from './components/calculator';
+import {Home, BasicRouting} from './components'
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+
 
 
 class App extends Component {
-	constructor(){
-		super();
-		this.age =20;
-		this.name="mola"
-		this.state = {name :"lolo",age : 30};
-	}
 	
-	getval(){
-		   return "aaaaa";
-	   }
-	onClick(){
-		this.setState({name : "BoB"});
-	}
-	setAge(e){
-		const age =e.target.value;
-		this.setState({age : age});
-	}
   render() {
-	   const myname="mike"; 
-	   const list =[<Header />,<Header />,<Header />]; 
+	   
 	
     return (
-	
+	<BrowserRouter>
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+         <ul>
+              <li><NavLink to="/" activeClassName="active">Home</NavLink></li>
+              <li><NavLink to="/BasicRouting" activeClassName="active">Basic Routing</NavLink></li>
+         </ul>
         </div>
-		<Header title="yyyyyyyy" name={this.state.name}/>
-		<Cal />
-		
-		
-		{ /* <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-		*/}
-		<p>{myname}</p>
-		<div>3+2 ={3+2}</div>
-		<div>call function{(() =>{return 1234})()}</div>
-		<div>{this.getval()}</div>
-		<div>Age: {this.age}</div>
-		<div>{this.state.name}</div>
-		<div>{this.state.age}</div>
-		<button onClick={this.onClick.bind(this)}>Click Me Please</button>
-		<input onChange={this.setAge.bind(this)} />
-		<Footer />
+		<div>
+		 
+		 <Switch>
+                        <Route path="/" component={Home} exact={true} />
+                        <Route path="/BasicRouting" component={BasicRouting} />
+                 
+                
+            </Switch>
+		</div>
       </div>
+	  </BrowserRouter>
     );
   }
 }
